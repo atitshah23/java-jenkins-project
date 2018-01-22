4da67df92ae9b6450ca189ed45a6d1c11b620aeb
Atit <atitshah23@gmail.com>
2018-01-22 19:22:03 +0530

    initial java commit

---
+++ src/Rectangle.java
@@ -0,0 +1,18 @@
+public class Rectangle {
+
+	public int width;
+	public int length
+
+	public Rectangle(int length, int width) {
+		this.length = length;
+		this.width  = width;
+	}
+
+	public int getArea(){
+		return length*width;
+	}
+
+	public int getPerimeter(){
+		return 2 * (length + width);
+	}
+}
